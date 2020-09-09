4add98ba4c3cc3cd158da388f7421fba3ead9697
McKenna Damschroder <mckdam@bu.edu>
2020-09-09 16:56:55 -0400

    Task 2 completed
    - Added code for task 2 into analysis.py
    - Added generated graphs as images into an images folder for use in the report later
    - Created Report.md to compile answers to each task

---
+++ Report.md
@@ -0,0 +1,31 @@
+## Responses
+
+Task 0:
+
+What is the greeting string issued by the server to the client upon first connecting?
+
+ - "ECE Senior Capstone IoT simulator""
+
+Task 2:
+
+What are the median and variance observed from the temperature data? 
+ - Median: 22.976429549397288
+ - Variance: 1019642.1939205542
+
+What are the median and variance observed from the occupancy data?
+ - Median: 5.0
+ - Variance: 64.87875984503773
+
+Plot the probability distribution function for each sensor type.
+NEED TO ADD (images are in images folder) Note: temperature has one called tempNoOutlier.png because it has an extreme outlier (38870.0176590916) that made the graph irrelevant. So the no outlier graph cuts off values at 100. I believe this cuts off that extreme outlier and one that is around 136(?).
+
+
+What is the mean and variance of the time interval of the sensor readings?
+ - Mean: 0.9693993684922244
+ - Variance: 0.86833733304211
+ 
+Plot the probability distribution function for the time intervals.
+NEED TO ADD (image in images folder)
+
+Does it mimic a well-known distribution for connection intervals in large systems?
+TO DO
--- analysis.py
+++ analysis.py
@@ -1,20 +1,54 @@
 import json
 from datetime import datetime
-import statistics
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
 	temperature = []
+	occupancy = []
+	co = []
+	timeInterval = []
 
+	time = -1;
 	with open('data.txt', "r") as f:
 		for line in f:
 			r = json.loads(line)
 			room = list(r.keys())[0]
-			time = datetime.fromisoformat(r[room]["time"])
+			if time == -1:
+				time = datetime.fromisoformat(r[room]["time"])
+			else:
+				prevTime = time;
+				time = datetime.fromisoformat(r[room]["time"])
+				timeInterval.append((time-prevTime).total_seconds())
 			temperature.append(r[room]["temperature"][0])
+			occupancy.append(r[room]["occupancy"][0])
+			co.append(r[room]["co2"][0])
+			
 
-	temperature.sort()
-	print(statistics.median(temperature))
-	print(statistics.variance(temperature))
+	tempData = pd.Series(temperature)
+	print("Temperature Median: ", tempData.median())
+	print("Temperature Variance: ", tempData.var())
+	ax = tempData.plot.kde()
+	plt.savefig("images/temp.png")
+	plt.clf()
+
+	occupData = pd.Series(occupancy)
+	print("Occupancy Median: ", occupData.median())
+	print("Occupancy Variance: ", occupData.var())
+	ax = occupData.plot.kde()
+	plt.savefig("images/occupancy.png")
+	plt.clf()
+	
+	coData = pd.Series(co)
+	ax = coData.plot.kde()
+	plt.savefig("images/co2.png")
+	plt.clf()
+
+	timeData = pd.Series(timeInterval)
+	print("Time Median: ", timeData.mean())
+	print("Time Variance: ", timeData.var())
+	ax = timeData.plot.kde()
+	plt.savefig("images/timeInterval.png")
 
 if __name__ == "__main__":
 	main()
